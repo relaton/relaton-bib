1f16a2f0ea3fb626864321ecc1d7d397741731b8
Nick Nicholas <opoudjis@optusnet.com.au>
2019-06-30 20:22:18 +1000

    reorg code

---
+++ lib/relaton_bib/biblio_version.rb
@@ -0,0 +1,35 @@
+module RelatonBib
+  # Version
+  class << self
+    def version_hash_to_bib(ret)
+      ret[:version] and ret[:version] =
+        RelatonBib::BibliographicItem::Version.new(
+          ret[:version][:revision_date], ret[:version][:draft])
+    end
+  end
+
+  class BibliographicItem
+    class Version
+      # @return [String]
+      attr_reader :revision_date
+
+      # @return [Array<String>]
+      attr_reader :draft
+
+      # @oaram revision_date [String]
+      # @param draft [Array<String>]
+      def initialize(revision_date = nil, draft = [])
+        @revision_date = revision_date
+        @draft         = draft
+      end
+
+      # @param builder [Nokogiri::XML::Builder]
+      def to_xml(builder)
+        builder.version do
+          builder.revision_date revision_date if revision_date
+          draft.each { |d| builder.draft d }
+        end
+      end
+    end
+  end
+end
