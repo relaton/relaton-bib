cdd17e8cd336cddda8a663ca33802168ee44d7df
Andrei Kislichenko <android.2net@gmail.com>
2020-02-12 16:24:56 +0700

    add test issue #18

--- spec/relaton_bib/localized_string_spec.rb
+++ spec/relaton_bib/localized_string_spec.rb
@@ -1,7 +1,18 @@
 RSpec.describe RelatonBib::LocalizedString do
-  subject { RelatonBib::LocalizedString.new "content", "en", "Latn" }
+  subject { RelatonBib::LocalizedString.new "content & character to escape", "en", "Latn" }
 
   it "returns false" do
     expect(subject.empty?).to be false  
+  end
+
+  it "escape &" do
+    xml = Nokogiri::XML::Builder.new do |b|
+      b.localized_string { subject.to_xml(b) }
+    end
+    expect(xml.doc.root.to_s).to be_equivalent_to <<~XML
+    <localized_string language="en" script="Latn">
+      content &amp;amp; character to escape
+    </localized_string>
+    XML
   end
 end
