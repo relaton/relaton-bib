2b88e64af846e47af4968b88889166f66d8ac6ad
Andrei Kislichenko <android.2net@gmail.com>
2020-05-16 18:27:48 +0200

    add abbreviation to stage & substage metanorma/metanorma-iso#342

--- lib/relaton_bib/document_status.rb
+++ lib/relaton_bib/document_status.rb
@@ -14,12 +14,12 @@ module RelatonBib
     # @return [String, NilClass]
     attr_reader :iteration
 
-    # @param stage [String]
-    # @param substage [String, NilClass]
+    # @param stage [String, RelatonBib::DocumentStatus::Stage]
+    # @param substage [String, NilClass, RelatonBib::DocumentStatus::Stage]
     # @param iteration [String, NilClass]
     def initialize(stage:, substage: nil, iteration: nil)
-      @stage = stage
-      @substage = substage
+      @stage = stage.is_a?(Stage) ? stage : Stage.new(stage)
+      @substage = substage.is_a?(Stage) ? substage : Stage.new(substage)
       @iteration = iteration
     end
 
@@ -27,18 +27,46 @@ module RelatonBib
     def to_xml(builder)
       builder.status do
         # FormattedString.instance_method(:to_xml).bind(status).call builder
-        builder.stage stage
-        builder.substage substage if substage
+        builder.stage { |b| stage.to_xml b }
+        builder.substage { |b| substage.to_xml b } if substage
         builder.iteration iteration unless iteration.to_s.empty?
       end
     end
 
     # @return [Hash]
     def to_hash
-      hash = { "stage" => stage }
-      hash["substage"] = substage if substage
+      hash = { "stage" => stage.to_hash }
+      hash["substage"] = substage.to_hash if substage
       hash["iteration"] = iteration if iteration
       hash
+    end
+
+    class Stage
+      # @return [String]
+      attr_reader :value
+
+      # @return [String, NilClass]
+      attr_reader :abbreviation
+
+      # @parma value [String]
+      # @param abbreviation [String, NilClass]
+      def initialize(value, abbreviation = nil)
+        @value = value
+        @abbreviation = abbreviation
+      end
+
+      # @param [Nokogiri::XML::Builder]
+      def to_xml(builder)
+        builder.parent[:abbreviation] = abbreviation if abbreviation
+        builder.text value
+      end
+
+      # @return [Hash]
+      def to_hash
+        hash = { "value" => value }
+        hash["abbreviation"] = abbreviation if abbreviation
+        hash
+      end
     end
   end
 end
