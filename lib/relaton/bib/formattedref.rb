f48e54fae71d66854838afbb8a8a989ac72aea6b
Andrei Kislichenko <android.2net@gmail.com>
2024-11-09 16:43:49 -0500

    WIP

--- lib/relaton/bib/formattedref.rb
+++ lib/relaton/bib/formattedref.rb
@@ -2,25 +2,11 @@ module Relaton
   module Bib
     class Formattedref
       # @return [String]
-      attr_reader :content
+      attr_accessor :content
 
       def initialize(content: nil)
         @content = content
       end
-
-      def content=(content)
-        @content = content.is_a?(String) ? content : content.to_xml
-      end
-
-      # @param content [Relaton::M
-      # def content=(content)
-      #   @content = content.is_a?(String) ? Model::TextElement.from_xml(content) : content
-      # end
-
-      # @param [Nokogiri::XML::Builder]
-      # def to_xml(builder)
-      #   builder.formattedref { super }
-      # end
 
       # @param prefix [String]
       # @return [String]
--- spec/relaton/model/formattedref_spec.rb
+++ spec/relaton/model/formattedref_spec.rb
@@ -6,19 +6,7 @@ describe Relaton::Model::Formattedref do
   it "parse XML" do
     formattedref = described_class.from_xml xml
     expect(formattedref).to be_instance_of Relaton::Bib::Formattedref
-    expect(formattedref.content).to be_instance_of Array
-    expect(formattedref.content.size).to eq 3
-    expect(formattedref.content[0]).to be_instance_of Relaton::Model::TextElement
-    expect(formattedref.content[1]).to be_instance_of Relaton::Model::TextElement
-    expect(formattedref.content[2]).to be_instance_of Relaton::Model::TextElement
-    expect(described_class.to_xml(formattedref)).to be_equivalent_to xml
-  end
-
-  it "parse XML" do
-    formattedref = Relaton::Bib::Formattedref.new
-    formattedref.content = "<strong>Strong</strong>content<em>Em</em>"
-    expect(formattedref.content).to be_instance_of Array
-    expect(formattedref.content.size).to eq 3
+    expect(formattedref.content).to eq "<strong>Strong</strong>Content<em>Em</em>"
     expect(described_class.to_xml(formattedref)).to be_equivalent_to xml
   end
 end
