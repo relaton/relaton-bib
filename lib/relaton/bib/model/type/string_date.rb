f98b32cfa5ecde73b34cd1d876d788b6d50f2ae0
Andrei Kislichenko <android.2net@gmail.com>
2025-11-09 12:31:02 -0500

    convert all dates into YYYY-MM-DD format

--- lib/relaton/bib/model/type/string_date.rb
+++ lib/relaton/bib/model/type/string_date.rb
@@ -5,11 +5,17 @@ module Relaton
     class StringDate < Lutaml::Model::Type::Value
       class Value
         extend Forwardable
+        extend Core::DateParser
 
         def_delegators :@value, :split, :to_s
 
         def initialize(value)
           @value = value
+        end
+
+        def self.parse_date(date)
+          value = super
+          new value if value
         end
 
         def to_date
@@ -22,7 +28,7 @@ module Relaton
       def self.cast(value)
         case value
         when Value then value
-        when String then Value.new(value)
+        when String then Value.parse_date(value)
         end
       end
 
--- spec/relaton/bib/model/type/string_date_spec.rb
+++ spec/relaton/bib/model/type/string_date_spec.rb
@@ -1,8 +1,22 @@
 describe Relaton::Bib::StringDate do
-  it "#to_date" do
-    sd = described_class.cast("2020-05-01")
-    expect(sd.to_date).to eq Date.new(2020, 5, 1)
-    sd2 = described_class.cast("invalid-date")
-    expect(sd2.to_date).to be_nil
+  describe ".cast" do
+    it "parses valid date" do
+      sd = described_class.cast("August 15, 2021")
+      expect(sd.to_s).to eq "2021-08-15"
+    end
+
+    it "returns nil for invalid date" do
+      sd = described_class::Value.parse_date("invalid-date")
+      expect(sd).to be_nil
+    end
+  end
+
+  describe "#to_date" do
+    context "valid date" do
+      it "returns Date object" do
+        sd = described_class.cast("2020-05-01")
+        expect(sd.to_date).to eq Date.new(2020, 5, 1)
+      end
+    end
   end
 end
