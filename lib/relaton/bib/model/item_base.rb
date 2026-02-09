2a6bd1f3442423005e8b5058386bf3ea4de2c032
Andrei Kislichenko <android.2net@gmail.com>
2026-02-08 23:47:39 -0500

    refactor ItemBase to use ItemBaseAttributes module for attribute removal

--- lib/relaton/bib/model/item_base.rb
+++ lib/relaton/bib/model/item_base.rb
@@ -1,19 +1,27 @@
 module Relaton
   module Bib
+    # Module to remove id, schema-version, fetched & ext attributes from Item subclasses.
+    # Used for bibitem/relation instances that don't need these attributes.
+    module ItemBaseAttributes
+      def self.included(base)
+        # we don't need id, schema-version & fetched attributes in relation/bibitem
+        base.mappings[:xml].instance_variable_get(:@attributes).delete("id")
+        base.mappings[:xml].instance_variable_get(:@attributes).delete("schema-version")
+        base.mappings[:xml].instance_variable_get(:@elements).delete("fetched")
+        base.mappings[:xml].instance_variable_get(:@elements).delete("ext")
+        base.attributes.delete :id
+        base.attributes.delete :schema_version
+        base.attributes.delete :fetched
+        base.attributes.delete :ext
+      end
+    end
+
     # The class is for relaton bibitem instances.
     # The in relaton bibitem instances dosn't have schema-version & fetched attributes.
     class ItemBase < Item
       model ItemData
 
-      # we don't need id, schema-version & fetched attributes in reation/bibitem
-      mappings[:xml].instance_variable_get(:@attributes).delete("id")
-      mappings[:xml].instance_variable_get(:@attributes).delete("schema-version")
-      mappings[:xml].instance_variable_get(:@elements).delete("fetched")
-      mappings[:xml].instance_variable_get(:@elements).delete("ext")
-      attributes.delete :id
-      attributes.delete :schema_version
-      attributes.delete :fetched
-      attributes.delete :ext
+      include ItemBaseAttributes
     end
   end
 end
